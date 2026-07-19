92075b2bc946b65d0e67201980c82c6b8648b953
Blueberry44477 <vas172674@gmail.com>
2026-07-19 20:44:32 +0300

    feat(navigation): implement responsive header component for mobile
    - Add HeaderComponent.
    - Create navigation configuration and model for navigation items.
    - Import modern-normalize and typography assets.

---
+++ src/app/core/config/navigation.config.ts
@@ -0,0 +1,9 @@
+import { NavItem } from "../models/navigation.model";
+
+export const NAVIGATION_ITEMS: NavItem[] = [
+  {route: '/home', label: 'Home'},
+  {route: '/destination', label: 'Destination'},
+  {route: '/crew', label: 'Crew'},
+  {route: '/technology', label: 'Technology'},
+];
+
---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, computed, signal } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { NAVIGATION_ITEMS } from '../config/navigation.config';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterLink],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  protected navItems = signal(NAVIGATION_ITEMS).asReadonly();
+  private readonly _isMenuOpen = signal<boolean>(false);
+  public readonly isMenuOpen = computed(() => this._isMenuOpen());
+
+  public toggleMenu() { this._isMenuOpen.update(state => !state); }
+  public closeMenu() {this._isMenuOpen.set(false); }
+}
